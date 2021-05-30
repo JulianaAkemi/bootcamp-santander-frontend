9946f1aca4e970ce6900faac7df86a777713709e
Juliana Akemi <julianaakkemi@gmail.com>
2021-05-30 15:35:33 -0300

    Frontend Santander Dev Week versão 1

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    DashboardPageComponent,
+    StockCardComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+  ],
+})
+export class DashboardModule { }
---
+++ src/app/shared/models/stock-model.ts
@@ -0,0 +1,11 @@
+class Stock {
+  id: number;
+  name: string;
+  price: number;
+  minimum: 0;
+  exclusiveMinimum: boolean;
+  variation: number;
+  date: string;
+}
+
+export default Stock;
